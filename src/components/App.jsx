e042abbdae060ee4ef3e1063b848620ccdc76658
chriskwong91 <chriskwong91@gmail.com>
2016-08-12 17:26:23 -0700

    Refactor app into a class component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,50 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentVideo: exampleVideoData[0],
+      videos: exampleVideoData
+      // playerVideo: function(index = 0) { return this.props.videos[index]},
+      // videoList: this.props.videos
+    };
+  }
+
+  changeVideo() {
+    this.setState({
+      currentVideo: function(index) { return exampleVideoData[index]; }
+    });
+  }
+
+  render() {
+    return (
+    <div>
+      <Nav />
+      <div className="col-md-7">
+        <VideoPlayer video={this.state.currentVideo}/>
+      </div>
+      <div className="col-md-5">
+        <VideoList videos={this.state.videos}/>
+      </div>
     </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+    );
+  }
+}
+// var App = () => (
+//   <div>
+//     <Nav />
+//     <div className="col-md-7">
+//       <VideoPlayer/>
+//     </div>
+//     <div className="col-md-5">
+//       <VideoList/>
+//     </div>
+//   </div>
+// );
+
+App.propTypes = {
+  videos: React.PropTypes.array.isRequired
+};
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
